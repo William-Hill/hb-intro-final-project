6141b4d5e02d0b9e9d9813e204837ef8c976b0ea
William Hill <mjh2225@gmail.com>
2015-09-30 20:11:05 -0700

    initial commit; made some classes so I track my money

---
+++ bank_account.py
@@ -0,0 +1,9 @@
+class Bank_Account(object):
+	def __init__(self,name,account_balance):
+		self.name = name
+		self.account_balance = account_balance
+	def deposit(self, money_deposited):
+		self.account_balance += money_deposited
+	def withdraw(self,money_withdrawn):
+		self.account_balance -= money_withdrawn
+
